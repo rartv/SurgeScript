c95845ca142f466d59cd6b759ab0df438b7b4ce0
ThinkPHP <ThinkPHP@gmail.com>
2020-11-19 03:05:04 +0800

    由于 Emby 对激活的状态有缓存，所以应该也不会被通知打扰，所以重新开启通知并修改通知内容

--- EmbyPremiere/EmbyPremiere.js
+++ EmbyPremiere/EmbyPremiere.js
@@ -1,6 +1,6 @@
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
     if($response.status!=200){
-        // $notification.post("Emby Premiere 已激活，有效期 999 天", "", "您可以选择关闭本模块了");
+        $notification.post("Emby Premiere 已激活，有效期 999 天", "", "");
         $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
     }else{
         $done({})
